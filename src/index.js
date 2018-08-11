4df512a7b7ee8de57f7c83b1cdab5efb39caabe9
jberg <jordannealberg@gmail.com>
2018-08-11 14:10:29 -0700

    add more shader functions

--- src/index.js
+++ src/index.js
@@ -435,3 +435,78 @@ export function prepareShader (shader) {
 
   return fullShader;
 }
+
+function isUserSampler (line) {
+  if (!_.startsWith(line, 'uniform sampler')) {
+    return false;
+  }
+
+  const builtinSamplers = [
+    'sampler_main',
+    'sampler_fw_main',
+    'sampler_pw_main',
+    'sampler_fc_main',
+    'sampler_pc_main',
+    'sampler_noise_lq',
+    'sampler_noise_lq_lite',
+    'sampler_noise_mq',
+    'sampler_noise_hq',
+    'sampler_pw_noise_lq',
+    'sampler_noisevol_lq',
+    'sampler_noisevol_hq',
+    'sampler_blur1',
+    'sampler_blur2',
+    'sampler_blur3'
+  ];
+
+  const re = /uniform sampler2D sampler_(.+);$/;
+  const matches = line.match(re);
+  if (matches && matches.length > 1) {
+    return !_.includes(builtinSamplers, `sampler_${matches[1]}`);
+  }
+
+  return false;
+}
+
+export function processOptimizedShader (shader) {
+  if (_.isEmpty(shader)) {
+    return '';
+  }
+
+  let processedShader = _.replace(shader, '#version 300 es\n', '');
+  processedShader = _.replace(processedShader, 'void main ()', 'shader_body');
+  processedShader = _.replace(processedShader, /highp\s*/g, '');
+  processedShader = _.replace(processedShader, /medp\s*/g, '');
+  processedShader = _.replace(processedShader, /lowp\s*/g, '');
+  processedShader = _.replace(processedShader, /xlv_TEXCOORD0/g, 'uv');
+  processedShader = _.replace(processedShader,
+                              /_glesFragData\[0\] = (.+);\n\}/,
+                              (match, varName) => `ret = ${varName}.xyz;\n}`);
+  processedShader = _.replace(processedShader, 'out vec4 _glesFragData[4];\n', '');
+
+  const shaderParts = getShaderParts(processedShader);
+  let fragShaderHeaderText = shaderParts[0];
+  let fragShaderText = shaderParts[1];
+
+  const shaderHeaderLines = _.split(fragShaderHeaderText, '\n');
+  const fileredHeaderLines = _.filter(shaderHeaderLines,
+                                      (line) => !(_.startsWith(line, 'in') ||
+                                                 (_.startsWith(line, 'uniform') &&
+                                                  !isUserSampler(line))));
+  fragShaderHeaderText = _.join(fileredHeaderLines, '\n');
+
+  let shaderBodyLines = _.split(fragShaderText, ';');
+  shaderBodyLines = _.dropWhile(shaderBodyLines, (line) => {
+    const trimmedLine = _.trim(line);
+    if (_.startsWith(trimmedLine, 'xlat_mutable')) {
+      const matches = trimmedLine.match(/xlat_mutable(.+)\s*=\s*(.+)/);
+      if (matches && matches.length === 3 && _.trim(matches[1]) === _.trim(matches[2])) {
+        return true;
+      }
+    }
+    return false;
+  });
+  fragShaderText = _.join(shaderBodyLines, ';');
+
+  return `${fragShaderHeaderText} shader_body { ${fragShaderText} }`;
+}
